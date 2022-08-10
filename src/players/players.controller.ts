edfb83cacc050c3c3bab196cb3764c8f503441f4
Breno <brenocartoon@hotmail.com>
2022-08-09 22:04:15 -0300

    feat: create post route to example service

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreatePlayerDto } from './dtos/player.dto';
+import { PlayersService } from './players.service';
 
 @Controller('api/v1/players')
-export class PlayersController {}
+export class PlayersController {
+  constructor(private readonly playerService: PlayersService) {}
+
+  @Post()
+  async createAndUpdatePlayer(@Body() playerData: CreatePlayerDto) {
+    return this.playerService.createUpdatePlayer(playerData);
+  }
+}
