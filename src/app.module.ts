35939f4e8c41a50c0d553a948bf535769f37d37d
Breno <brenocartoon@hotmail.com>
2022-08-12 00:00:40 -0300

    chore: setting up mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlayersModule } from './players/players.module';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    MongooseModule.forRoot(process.env.DB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    PlayersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/players/players.module.ts
+++ src/players/players.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersController } from './players.controller';
 import { PlayersService } from './players.service';
+import { PlayerSchema } from './schema/players.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Player', schema: PlayerSchema }]),
+  ],
   controllers: [PlayersController],
   providers: [PlayersService],
 })
