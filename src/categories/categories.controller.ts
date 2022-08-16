61bf833b88e02c504801d9dedcb446161559a51f
Breno <brenocartoon@hotmail.com>
2022-08-15 22:01:02 -0300

    feat: add create category route

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dtos/categories.dto';
 
 @Controller('api/v1/categories')
 export class CategoriesController {
   constructor(private readonly categoryService: CategoriesService) {}
+
+  @Post()
+  async createCategory(@Body() categoryData: CreateCategoryDto) {
+    return this.categoryService.createCategory(categoryData);
+  }
 }
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,6 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { CreateCategoryDto } from './dtos/categories.dto';
 import { ICategory } from './interfaces/categories.interface';
 
 @Injectable()
@@ -9,4 +14,15 @@ export class CategoriesService {
     @InjectModel('Category')
     private readonly categoryModel: Model<ICategory>,
   ) {}
+
+  async createCategory(categoryData: CreateCategoryDto): Promise<ICategory> {
+    const { category } = categoryData;
+    const foundCategory = await this.categoryModel.findOne({ category }).exec();
+
+    if (foundCategory)
+      throw new BadRequestException(`Category ${category} already exists!`);
+
+    const categoryToCreate = new this.categoryModel(categoryData);
+    return await categoryToCreate.save();
+  }
 }
