705d76a7a1f2d4b9ada43c4a8786e26b4c43cd73
Breno <brenocartoon@hotmail.com>
2022-08-22 19:12:16 -0300

    fix: add missing fields

--- src/challenges/interfaces/challenges.interface.ts
+++ src/challenges/interfaces/challenges.interface.ts
@@ -3,6 +3,11 @@ import { IPlayer } from 'src/players/interfaces/players.interface';
 
 export interface IChallenge extends Document {
   readonly challengeDate: Date;
+  status: string;
+  requestDate: Date;
+  responseDate: Date;
   challenger: string;
+  category: string;
   players: Array<IPlayer>;
+  match: string;
 }
